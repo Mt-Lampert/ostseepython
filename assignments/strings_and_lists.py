9a290ed5de1ccf3768323dffe48674f4d2de6ba5
Mt-Lampert <perrin.blog@web.de>
2020-09-03 15:45:55 +0200

    added first assignments for strings and lists

---
+++ assignments/strings_and_lists.py
@@ -0,0 +1,23 @@
+
+def laengeDerElemente(dieListe):
+    ausgabe = []
+    #
+    # Platz fuer deinen Code
+    #
+    return ausgabe
+
+
+
+def filtereElementeMitG(dieListe):
+    ausgabe = []
+    #
+    # Platz fuer deinen Code
+    #
+    return ausgabe
+
+def generiereCSV(dieListe):
+    ausgabe = ""
+    #
+    # Platz fuer deinen Code
+    #
+    return ausgabe
